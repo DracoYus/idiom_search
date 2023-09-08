a409522a4e715224969e0243befd1ad5147ed60a
DracoYu <876319691@qq.com>
2023-09-08 18:15:34 +0800

    refactor: 修改入参为列表，并重构内部算法

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import json
 from pypinyin import lazy_pinyin, Style
 import re
 
-name_first = "马云"
-name_second = "李彦宏"
-pinyin_first = lazy_pinyin(name_first, style=Style.TONE)
-pinyin_second = lazy_pinyin(name_second, style=Style.TONE)
+name_list = ["马云", "李彦宏"]
+pinyin_list = [lazy_pinyin(name, style=Style.TONE) for name in name_list]
 file_path = "./idiom.json"
 
 out = ""
@@ -17,38 +15,44 @@ out = ""
 # 5、【u】ū ú ǔ ù
 # 6、【ü】ǖ ǘ ǚ ǜ
 
-with open(file_path, "r", encoding="utf-8") as f:
-    idiom_json = json.load(f)
+def load_idiom(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        idiom_json = json.load(f)
+    return idiom_json
 
 
-for i in idiom_json:
-    idiom_pinyin = re.split("[， ]", i["pinyin"])
-    pinyin_first_in_idiom = [pinyin in idiom_pinyin for pinyin in pinyin_first]
-    pinyin_second_in_idiom = [pinyin in idiom_pinyin for pinyin in pinyin_second]
-    if any(pinyin_first_in_idiom) and any(pinyin_second_in_idiom):
-        comma_index = i["word"].index("，") if "，" in i["word"] else None
-        idiom_word = i["word"].replace("，", "")
-        pinyin_first_index = pinyin_first_in_idiom.index(True)
-        pinyin_second_index = pinyin_second_in_idiom.index(True)
-        idiom_word_index_first = idiom_pinyin.index(pinyin_first[pinyin_first_index])
-        idiom_word_index_second = idiom_pinyin.index(pinyin_second[pinyin_second_index])
-        idiom_word = (
-            idiom_word[:idiom_word_index_first]
-            + name_first[pinyin_first_index]
-            + idiom_word[idiom_word_index_first + 1 :]
-        )
-        idiom_word = (
-            idiom_word[:idiom_word_index_second]
-            + name_second[pinyin_second_index]
-            + idiom_word[idiom_word_index_second + 1 :]
-        )
-        idiom_word = (
-            idiom_word[:comma_index] + "，" + idiom_word[comma_index:]
-            if comma_index
-            else idiom_word
-        )
-        out += idiom_word + "\n"
+def get_idiom_result(pinyin_list, idiom_json):
+    result = ""
+    for i in idiom_json:
+        idiom_pinyin = re.split("[， ]", i["pinyin"])
+        word_in_idiom_list = [[find_index(idiom_pinyin, word) for word in pinyin] for pinyin in pinyin_list]
+        if all([any(index >= 0 for index in word_in_idiom) for word_in_idiom in word_in_idiom_list]):
+            comma_index = i["word"].index("，") if "，" in i["word"] else None
+            idiom_word = i["word"].replace("，", "")
+            for idx, word_in_idiom in enumerate(word_in_idiom_list):
+                for index, item in enumerate(word_in_idiom):
+                    if item >=0:
+                        idiom_word = idiom_word[:item] + name_list[idx][index] + idiom_word[item + 1 :]
+            idiom_word = (
+                idiom_word[:comma_index] + "，" + idiom_word[comma_index:]
+                if comma_index
+                else idiom_word
+            )
+            result += idiom_word + "\n"
+    return result
 
+def find_index(list, element):
+    try:
+        index = list.index(element)
+    except ValueError:
+        index = -1
+    return index
 
-with open("out.txt", "w", encoding="utf-8") as f:
-    f.write(out)
+def write_to_file(result):
+    with open("out.txt", "w", encoding="utf-8") as f:
+        f.write(result)
+
+if __name__ == "__main__":
+    idiom_json = load_idiom(file_path)
+    result = get_idiom_result(pinyin_list, idiom_json)
+    write_to_file(result)
